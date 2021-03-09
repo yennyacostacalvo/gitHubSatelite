b23945db4d7af44d2953ea0465d67ba405967b16
yennyacostacalvo <69409181+yennyacostacalvo@users.noreply.github.com>
2021-03-09 16:26:55 -0500

    Delete servicio-satelites/src/main/java/com/app/satelite/repository directory

--- servicio-satelites/src/main/java/com/app/satelite/repository/SateliteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.satelite.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.app.satelite.entity.Satelite;
-
-public interface SateliteRepository extends CrudRepository<Satelite,Long> {
-
-}
--- servicio-satelites/src/main/java/com/app/satelite/repository/UbicacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.satelite.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.app.satelite.entity.Ubicacion;
-
-public interface UbicacionRepository extends CrudRepository<Ubicacion,Long>{
-
-}

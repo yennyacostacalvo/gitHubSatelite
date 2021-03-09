b3ebbd1b7d788fcf2b4709725effae1927365d08
Yenny Acosta <yennyacosta.calvo@gmail.com>
2021-03-09 16:20:44 -0500

    Ajuste intento2

--- servicio-satelites/src/main/java/com/app/satelite/controller/SateliteController.java
+++ servicio-satelites/src/main/java/com/app/satelite/controller/SateliteController.java
@@ -1,19 +1,15 @@
 package com.app.satelite.controller;
 
 
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.satelite.dto.MensajeDto;
-import com.app.satelite.dto.SateliteDto;
-import com.app.satelite.service.MensajeService;
-import com.app.satelite.service.SateliteService;
+import com.app.satelite.entity.Satellite;
+import com.app.satelite.serviceImpl.SatelliteServiceImpl;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,33 +19,16 @@ import lombok.extern.slf4j.Slf4j;
 public class SateliteController {	
 	
 	@Autowired
-	private SateliteService sateliteService;
+	private SatelliteServiceImpl serviceSatelite;
 	
-	@Autowired
-	private MensajeService mensajeService;
-	
-	
-	
+
 	@GetMapping("/topSecret")
-	public List<SateliteDto> listarDto(){
-		return sateliteService.findAllAll().stream().map(producto ->{
-			SateliteDto dto =new SateliteDto();
-			dto.setName(producto.getName());
-			dto.setDistance(producto.getDistance());
-			dto.setMensaje(obtenerMensaje(producto.getId()));
-			return dto;
-		}).collect(Collectors.toList());
+	public List<Satellite> listarSatelites(){
+		return serviceSatelite.getAllSatellites();
 	}
 	
 	
-	public List<MensajeDto>  obtenerMensaje(Long idSatelite) {
-		MensajeDto mensaje = new MensajeDto();
-		return mensajeService.findById(idSatelite).stream().map(producto ->{
-			mensaje.setMensaje(producto.getMensaje());
-			return mensaje;
-		}).collect(Collectors.toList());
-	}
-	
+
 	
 	
 	
